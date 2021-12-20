049c8df6c9ece0b725f4b22b0e1f958610924565
Suraj Kr <surajkr739@gmail.com>
2021-12-20 22:27:40 +0530

    Add files via upload
    1. Program to find sum of two numbers.

---
+++ 1.c
@@ -0,0 +1,15 @@
+//1.Program to find the sum of  two  number
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b,sum;
+	
+	printf("Enter the two number");
+	scanf("%d\n%d", &a,&b);
+	
+	sum = a+b;
+	
+	printf("The sum of two Num %d",sum);
+	return 0;
+}
